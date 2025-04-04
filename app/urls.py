cb49a3edc5b4466cee0b17213c7541f71dbcad19
LuizBlume <luiz.fernando.251107@gmail.com>
2025-04-04 15:09:26 -0300

    FEAT: Criação de API para editora.

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,4 @@
+from posixpath import basename
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -7,12 +8,13 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet, CategoriaViewSet
+from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet
 
 router = DefaultRouter()
 
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
+router.register(r"editoras", EditoraViewSet, basename='editoras')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
