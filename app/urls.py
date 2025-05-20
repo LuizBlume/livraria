47381cbaa4bd910e735e9e77b30e138a165b0936
LuizBlume <luiz.fernando.251107@gmail.com>
2025-05-20 08:56:08 -0300

    FEAT: Image uploader finished.

--- app/urls.py
+++ app/urls.py
@@ -9,6 +9,10 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 
 from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+from uploader.router import router as uploader_router
+
 
 router = DefaultRouter()
 
@@ -32,6 +36,7 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name='schema'),
         name='redoc',
     ),
-    # API
+    path("api/media/", include(uploader_router.urls)),
     path('api/', include(router.urls)),
 ]
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
