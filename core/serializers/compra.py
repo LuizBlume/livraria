9541508cfe61ac0c277a36db37c33691786ab7a9
LuizBlume <luiz.fernando.251107@gmail.com>
2025-08-26 09:03:46 -0300

    feat: criação de uma compra a partir do usuário autenticado #29

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -2,28 +2,10 @@ from rest_framework.serializers import CharField, ModelSerializer, SerializerMet
 
 from core.models import Compra, ItensCompra
 
-class ItensCompraSerializer(ModelSerializer):
-    total = SerializerMethodField()
-
-    def get_total(self, instance):
-        return instance.livro.preco * instance.quantidade
-
-    class Meta:
-        model = ItensCompra
-        fields = ('livro', 'quantidade', 'total')
-        depth = 1
-        
 class ItensCompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
         fields = ('livro', 'quantidade')
-
-class CompraSerializer(ModelSerializer):
-    status = CharField(source='get_status_display', read_only=True)
-    itens = ItensCompraSerializer(many=True, read_only=True)
-    class Meta:
-        model = Compra
-        fields = ('id', 'usuario', 'status', 'total', 'itens')
         
 class CompraCreateUpdateSerializer(ModelSerializer):
     itens = ItensCompraCreateUpdateSerializer(many=True)
@@ -32,6 +14,14 @@ class CompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = ('usuario', 'itens')
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop('itens')
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
 
     def update(self, compra, validated_data):
         itens_data = validated_data.pop('itens', [])
@@ -56,3 +46,22 @@ class CompraListSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = ('id', 'usuario', 'itens')
+
+class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+
+    def get_total(self, instance):
+        return instance.livro.preco * instance.quantidade
+
+    class Meta:
+        model = ItensCompra
+        fields = ('livro', 'quantidade', 'total')
+        depth = 1
+
+class CompraSerializer(ModelSerializer):
+    status = CharField(source='get_status_display', read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
+    class Meta:
+        model = Compra
+        fields = ('id', 'usuario', 'status', 'total', 'itens')
+    
